4a02198e47afcc35a0321bb7678218f365197ef5
Steve Lomeli <rlomeli158@gmail.com>
2021-03-20 14:42:18 -0500

    Uploaded Files

---
+++ Web App Development - C# and HTML/Personal Website/Personal Website/Lomeli_Steve_HW0/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lomeli_Steve_HW0.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Lomeli_Steve_HW0.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+        public IActionResult ListResponses()
+        {
+            IEnumerable<GuestResponse> attendeeList =
+                                            Repository.Responses.Where(r => r.WillAttend == true);
+            return View(attendeeList);
+        }
+
+    }
+}
---
+++ Web App Development - C# and HTML/Personal Website/Personal Website/Lomeli_Steve_HW0/Models/Repository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lomeli_Steve_HW0.Models
+{
+    public static class Repository
+    {
+        private static List<GuestResponse> responses = new List<GuestResponse>(); //constructor
+
+        public static IEnumerable<GuestResponse> Responses
+        {
+            get
+            {
+                return responses;
+            }
+        }
+
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
